b33ea40531f55eaaa1c2fc926ed263b656657fbc
litingzhang <704219007@qq.com>
2026-01-25 11:07:35 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
+SYSROOT="$(cd ../sysroots/cm5 && pwd)"
+export PATH="$(echo "$PATH" | tr ':' '\n' | grep -v "$SYSROOT/usr/bin" | paste -sd ':' -)"
 if [ "$1" = "all" ]; then
     echo "build ORB-SLAM"
     cd ../ORB_SLAM3-master
     rm -rf build
-    cmake -S . -B build
+    cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=$SYSROOT/toolchain-cm5-aarch64.cmake
     cmake --build build -j16
+    cd -
 fi
-
-SYSROOT="$(cd ../sysroots/cm5 && pwd)"
-export PATH="$(echo "$PATH" | tr ':' '\n' | grep -v "$SYSROOT/usr/bin" | paste -sd ':' -)"
 
 rm -rf build
 
