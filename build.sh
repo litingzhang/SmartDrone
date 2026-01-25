8f6291c19cd250ebfe3f49e18969b18076216043
litingzhang <704219007@qq.com>
2026-01-25 10:48:52 +0800

    add orb slam compile command

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "$1" = "all" ]; then
+    echo "build ORB-SLAM"
+    cd ../ORB_SLAM3-master
+    rm -rf build
+    cmake -S . -B build
+    cmake --build build -j16
+fi
+
 SYSROOT="$(cd ../sysroots/cm5 && pwd)"
 export PATH="$(echo "$PATH" | tr ':' '\n' | grep -v "$SYSROOT/usr/bin" | paste -sd ':' -)"
 
