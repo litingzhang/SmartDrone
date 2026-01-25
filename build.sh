1cef54c3782de8643bf1e3a8446b829f53f168c9
litingzhang <704219007@qq.com>
2026-01-25 11:44:25 +0800

    fix compile

--- build.sh
+++ build.sh
@@ -7,12 +7,12 @@ echo "SYSROOT:$SYSROOT"
 if [ "$1" = "all" ]; then
     echo "build ORB-SLAM"
     cd ../ORB_SLAM3-master
-    rm -rf build
-    cmake -S . -B build -DSYSROOT=$SYSROOT -DCMAKE_TOOLCHAIN_FILE=$SCRIPT_DIR/toolchain/toolchain-cm5-aarch64.cmake
-    cmake --build build -j16
+    rm -rf temp
+    cmake -S . -B temp -DSYSROOT=$SYSROOT -DCMAKE_TOOLCHAIN_FILE=$SCRIPT_DIR/toolchain/toolchain-cm5-aarch64.cmake
+    cmake --build temp -j16
     cd -
 fi
 
-rm -rf build
-cmake -S . -B build -DSYSROOT="$SYSROOT" -DPKG_CONFIG_EXECUTABLE=/usr/bin/pkg-config
-cmake --build build -j16
+rm -rf temp
+cmake -S . -B temp -DSYSROOT="$SYSROOT" -DPKG_CONFIG_EXECUTABLE=/usr/bin/pkg-config
+cmake --build temp -j16
